b31ea8514f3264dcf29463761f6227da6980083a
“Adham <adham.m.abdulah@gmail.com>
2025-07-06 18:05:19 +0300

    MyAppBar widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,41 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:habitly/constants/colors.dart';
+import 'package:habitly/widgets/my_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: maincolor,
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "My Habits",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-                ),
-                // SizedBox(height: 4),
-                Text(
-                  "Track your daily progress",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xff666666),
-                  ),
-                ),
-              ],
-            ),
-            CircleAvatar(backgroundColor: Color(0xff618EF7)),
-          ],
-        ),
+      body: Column(
+        children: [
+          MyAppBar(),
+        ],
       ),
     );
   }
 }
+
