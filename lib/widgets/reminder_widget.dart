0002e114047e214a815332325669ed38062865aa
“Adham <adham.m.abdulah@gmail.com>
2025-07-08 20:08:28 +0300

    ReminderWidget Done

--- lib/widgets/reminder_widget.dart
+++ lib/widgets/reminder_widget.dart
@@ -1,6 +1,9 @@
+import 'package:day_night_time_picker/lib/daynight_timepicker.dart';
+import 'package:day_night_time_picker/lib/state/time.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 import 'package:habitly/constants/colors.dart';
+import 'package:habitly/widgets/my_text_field.dart';
 
 class ReminderWidget extends StatefulWidget {
   const ReminderWidget({super.key});
@@ -11,34 +14,88 @@ class ReminderWidget extends StatefulWidget {
 
 class _ReminderWidgetState extends State<ReminderWidget> {
   var reminderEnabled = true;
+  Time _time = Time(hour: 10, minute: 30, second: 0);
+  void _onTimeChanged(Time newTime) {
+    setState(() {
+      _time = newTime;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         children: [
-          const Text(
-            "Reminder",
-            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Reminder",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              FlutterSwitch(
+                width: 50.0,
+                height: 26.0,
+                valueFontSize: 12.0,
+                toggleSize: 22.0,
+                value: reminderEnabled,
+                borderRadius: 15.0,
+                padding: 2.0,
+                showOnOff: false,
+                activeColor: myclickcolor,
+                inactiveColor: Colors.grey.shade300,
+                onToggle: (val) {
+                  setState(() {
+                    reminderEnabled = val;
+                  });
+                },
+              ),
+            ],
           ),
-          FlutterSwitch(
-            width: 50.0,
-            height: 26.0,
-            valueFontSize: 12.0,
-            toggleSize: 22.0,
-            value: reminderEnabled,
-            borderRadius: 15.0,
-            padding: 2.0,
-            showOnOff: false,
-            activeColor: myclickcolor,
-            inactiveColor: Colors.grey.shade300,
-            onToggle: (val) {
-              setState(() {
-                reminderEnabled = val;
-              });
-            },
+          SizedBox(height: 5),
+          Visibility(
+            visible: reminderEnabled,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  showPicker(
+                    context: context,
+                    value: _time,
+                    sunrise: TimeOfDay(hour: 6, minute: 0),
+                    sunset: TimeOfDay(hour: 18, minute: 0),
+                    duskSpanInMinutes: 120,
+                    onChange: _onTimeChanged,
+                  ),
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: myclickcolor, width: 1),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.alarm, color: myclickcolor, size: 22),
+                    SizedBox(width: 10),
+                    Text(
+                      "${_time.hourOfPeriod.toString().padLeft(2, '0')}:${_time.minute.toString().padLeft(2, '0')} ${_time.period == DayPeriod.am ? 'AM' : 'PM'}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: myclickcolor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
